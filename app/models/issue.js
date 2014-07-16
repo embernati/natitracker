e9e7b7187bd79152c271d66bfb8d6ebcf744bd69
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-07-16 19:36:15 -0400

    Remove old fixture data

--- app/models/issue.js
+++ app/models/issue.js
@@ -4,11 +4,4 @@ var Issue = DS.Model.extend({
   title: DS.attr('string')
 });
 
-Issue.reopenClass({
-  FIXTURES: [
-    { id: 1, title: 'first' },
-    { id: 2, title: 'second' }
-  ]
-});
-
 export default Issue;
